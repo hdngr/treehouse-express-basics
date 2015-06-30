3ff9ac14ddb940f4ae4666d674f12f0c160e2f35
Huston Hedinger <hstnhdngr@gmail.com>
2015-06-30 10:40:16 -0700

    Start of Stage 1 Video 4

--- src/app.js
+++ src/app.js
@@ -1,3 +1,11 @@
 'use strict';
 
 var express = require('express');
+
+var app = express();
+
+app.get('/', function(request, response){
+	response.send("I Love Treehouse!");
+});
+
+app.listen(3000);
