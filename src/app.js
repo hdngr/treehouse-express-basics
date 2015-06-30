1b2866cff9ab9485dafc52c0aa8c6bd0e59a708c
Huston Hedinger <hstnhdngr@gmail.com>
2015-06-30 10:50:16 -0700

    start of stage 2 video 1

--- src/app.js
+++ src/app.js
@@ -4,8 +4,10 @@ var express = require('express');
 
 var app = express();
 
-app.get('/', function(request, response){
-	response.send("I Love Treehouse!");
+app.get('/', function(req, res){
+	res.send("<h1>I Love Treehouse!</h1>");
 });
 
-app.listen(3000);
+app.listen(3000, function(){
+	console.log("The frontend server is running on port 3000!")
+});
