f59aa071a9cf4d7961c352436213b27546c79832
Huston Hedinger <hstnhdngr@gmail.com>
2015-07-02 11:54:01 -0700

    start of stage 2 video 2

--- src/app.js
+++ src/app.js
@@ -1,11 +1,16 @@
 'use strict';
 
-var express = require('express');
+var express = require('express'),
+	  posts = require('./mock/posts.json');
 
 var app = express();
 
 app.get('/', function(req, res){
 	res.send("<h1>I Love Treehouse!</h1>");
+});
+
+app.get('/blog', function(req, res){ 
+	res.send(posts);
 });
 
 app.listen(3000, function(){
