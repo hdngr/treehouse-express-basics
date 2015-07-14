2dfe2147b342e6e8fdf6f7e0cd49fc05a1586148
Huston Hedinger <hstnhdngr@gmail.com>
2015-07-14 10:07:26 -0700

    beginning of Stage 5 Video 2

--- src/app.js
+++ src/app.js
@@ -5,6 +5,8 @@ var express = require('express'),
 
 var app = express();
 
+app.use('/static', express.static(__dirname + '/public'))
+
 app.set('view engine', 'jade');
 app.set('views', __dirname + '/templates');
 
