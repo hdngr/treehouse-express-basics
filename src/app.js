3c50934231ee89ba756af26b3352c3919facaa5a
Huston Hedinger <hstnhdngr@gmail.com>
2015-07-20 09:53:09 -0700

    beginning of Stage 6 Video 2

--- src/app.js
+++ src/app.js
@@ -3,6 +3,9 @@
 var express = require('express'),
 	  posts = require('./mock/posts.json');
 
+var postsLists = Object.keys(posts).map(function(value) {
+							         return posts[value]})
+
 var app = express();
 
 app.use('/static', express.static(__dirname + '/public'))
@@ -18,7 +21,7 @@ app.get('/blog/:title?', function(req, res){
 	var title = req.params.title;
 	if (title === undefined) {
 		res.status(503);
-		res.send("This page is under construction!");
+		res.render('blog', {posts: postsLists})
 	} else {
 		var post = posts[title] || {};
 		res.render('post', { post: post});
